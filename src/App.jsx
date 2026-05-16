bc55bfb71d923ceabe72f9d0d66cc250f9319bec
Arjun Ajjampur <arjun.ajjampur@gmail.com>
2026-05-16 14:25:57 -0700

    Scroll to top on route change; fix card border contrast in light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
 import Process from './pages/Process';
@@ -15,9 +16,16 @@ import AIWorkshop from './pages/work/AIWorkshop';
 import LLMAssistant from './pages/work/LLMAssistant';
 import './styles.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
